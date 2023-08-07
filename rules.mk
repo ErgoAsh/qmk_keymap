296e49d4f81c7b7ae2c2854770567e7f56cc15ec
Marek K <ErgoAsh@users.noreply.github.com>
2023-08-07 17:33:59 +0200

    Push keyboard mapping files

---
+++ rules.mk
@@ -0,0 +1,14 @@
+SRC += source.c
+
+MOUSEKEY_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
+OLED_DRIVER     = SSD1306
+
+SPLIT_KEYBOARD  = yes
+
+LTO_ENABLE      = yes    # Enable Link Time Optimization for saving space
+COMMAND_ENABLE   = no    # Disable commands for debug and configuration
+
---
+++ source.c
@@ -0,0 +1,23 @@
+#include QMK_KEYBOARD_H
+
+#include <stdio.h>
+#include "keycodes.h"
+#include "keycode.h"
+#include "process_key_override.h"
+
+const key_override_t T_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_QUOT, KC_LPRN, (1 << _ENGRAM)); // ' -> (
+const key_override_t Y_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_DQUO, KC_RPRN, (1 << _ENGRAM)); // " -> ^
+
+const key_override_t G_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_COMM, KC_SCLN, (1 << _ENGRAM)); // , -> ;
+const key_override_t H_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_DOT,  KC_COLN, (1 << _ENGRAM)); // . -> :
+
+const key_override_t B_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_MINS, KC_UNDS, (1 << _ENGRAM)); // - -> _
+const key_override_t N_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_QUES, KC_EXLM, (1 << _ENGRAM)); // ? -> !
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &T_override, &Y_override,
+    &G_override, &H_override,
+    &B_override, &N_override,
+    NULL // Null terminate the array of overrides!
+};
