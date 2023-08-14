30b700ac9cfd497bfbe9923e4f26af1dc700c3fd
Marek K <ErgoAsh@users.noreply.github.com>
2023-08-14 09:30:24 +0200

    Fix gaming layer

--- config.h
+++ config.h
@@ -22,8 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define EE_HANDS
 
-//#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 250
+#define TAPPING_TERM 75
 
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
