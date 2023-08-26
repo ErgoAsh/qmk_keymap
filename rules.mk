3ee436b1626bfc22ef8398069caf3a9185159435
Marek K. <ErgoAsh@users.noreply.github.com>
2023-08-26 09:38:13 +0200

    Disable code optimizations

--- rules.mk
+++ rules.mk
@@ -4,11 +4,11 @@ MOUSEKEY_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 
 RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
+OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
 
 SPLIT_KEYBOARD  = yes
 
-LTO_ENABLE      = yes    # Enable Link Time Optimization for saving space
+LTO_ENABLE      = no    # Enable Link Time Optimization for saving space
 COMMAND_ENABLE   = no    # Disable commands for debug and configuration
 
